ecec75cbbf7a872339a401ed4479fe332ef3bce5
Ana Guerra <anamaria.guerra@icontainers.com>
2023-03-10 10:47:57 +0000

    mongoose

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   const { id } = request.params
 
   Note.findById(id).then(foundedNote => {
@@ -34,15 +34,34 @@ app.get('/api/notes/:id', (request, response) => {
     } else {
       response.status(404).end()
     }
+  }).catch(error => {
+    next(error)
   })
 })
 
-// app.delete('/api/notes/:id', (request, response) => {
-//   const id = Number(request.params.id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { id } = request.params
+  const note = request.body
+  const updatedNote = {
+    content: note.content,
+    important: note.important
+  }
 
-//   notes = notes.filter(note => note.id !== id)
-//   response.status(204).end()
-// })
+  Note.findByIdAndUpdate(id, updatedNote, { new: true })
+    .then(result => {
+      response.status(200).json(result)
+    })
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  const { id } = request.params
+
+  Note.findByIdAndRemove(id).then(result => {
+    response.status(204).end()
+  }).catch(error => {
+    next(error)
+  })
+})
 
 app.post('/api/notes', (request, response) => {
   const note = request.body
@@ -64,11 +83,16 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-// app.use((request, response) => {
-//   response.status(404).json({
-//     error: 'Not found'
-//   })
-// })
+/** Middleware */
+app.use((error, request, response, next) => {
+  console.error(error)
+
+  if (error.name === 'CastError') {
+    response.status(400).send({ error: 'invalid id type' })
+  } else {
+    response.status(500).end()
+  }
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${PORT}`)
