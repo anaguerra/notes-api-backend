c8147e426c8550d048cab12561a0fb05808d004e
Ana Guerra <anamaria.guerra@icontainers.com>
2023-03-14 10:02:41 +0000

    create user

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import User from '../models/User.js'
+import bcrypt from 'bcrypt'
 
 const usersRouter = express.Router()
 
@@ -7,15 +8,17 @@ usersRouter.post('/', async (request, response) => {
   const { body } = request
   const { username, name, password } = body
 
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
   const user = new User({
     username,
     name,
-    passwordHash: password
+    passwordHash
   })
 
   const savedUser = await user.save()
 
-  response.json(savedUser)
+  response.status(201).json(savedUser)
 })
 
 export default usersRouter
---
+++ tests/users.test.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+import { server } from '../index.js'
+import User from '../models/User.js'
+import { api } from './helpers.js'
+import bcrypt from 'bcrypt'
+
+beforeEach(async () => {
+  await User.deleteMany({})
+
+  // create always a root user
+  const passwordHash = await bcrypt.hash('pwsd', 10)
+  const user = new User({ username: 'anaroot', passwordHash })
+  await user.save()
+})
+
+describe('POST new user', () => {
+  test('create new user', async () => {
+    const usersDB = await User.find({})
+    const usersAtStart = usersDB.map(user => user.toJSON())
+    const newUser = {
+      username: 'anaguerra',
+      name: 'Ana',
+      password: '12345'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersDBAfter = await User.find({})
+    const usersAtEnd = usersDBAfter.map(user => user.toJSON())
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(user => user.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+  server.close()
+})
